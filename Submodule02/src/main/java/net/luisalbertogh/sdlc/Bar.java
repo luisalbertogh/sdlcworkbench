886410f784e69d27813c9593de0031eade694705
luisalbertogh <luisalbertogh@hotmail.com>
2016-06-15 16:11:35 +0200

    Bar class.

---
+++ Submodule02/src/main/java/net/luisalbertogh/sdlc/Bar.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package net.luisalbertogh.sdlc;
+
+/**
+ * @author loga
+ *
+ */
+public final class Bar {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, I'm BAR...");
+		System.exit(0);
+	}
+
+}
