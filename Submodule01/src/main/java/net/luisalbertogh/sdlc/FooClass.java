84324f0598d5142c1c8762a36dc236a4db9b8dd9
luisalbertogh <luisalbertogh@hotmail.com>
2016-06-15 11:30:31 +0200

    Initial version.

---
+++ Submodule01/src/main/java/net/luisalbertogh/sdlc/FooClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package net.luisalbertogh.sdlc;
+
+/**
+ * @author garcluia
+ *
+ */
+public final class FooClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("I am calling from Submodule 01 - FO");
+		System.exit(0);
+	}
+
+}
