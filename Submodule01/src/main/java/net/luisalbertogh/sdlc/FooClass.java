8901e7564eee4e2b55346955b83f674e5eed7a5e
luisalbertogh <luisalbertogh@hotmail.com>
2016-06-21 14:45:44 +0200

    Change in submodule 01 only.

--- Submodule01/src/main/java/net/luisalbertogh/sdlc/FooClass.java
+++ Submodule01/src/main/java/net/luisalbertogh/sdlc/FooClass.java
@@ -13,7 +13,7 @@ public final class FooClass {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("I am calling from Submodule 01");
+		System.out.println("I am calling from Submodule 01 - New change");
 		System.exit(0);
 	}
 
